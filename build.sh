13424fa117a6920c528d799a00f9ed98ffcab49c
sam543381 <sam543381@pm.me>
2019-08-04 15:22:20 +0200

    build.sh rewrite (bug fixes & cleaner) + configuration support

--- build.sh
+++ build.sh
@@ -1,45 +1,51 @@
 #!/bin/sh
 
+# CONFIG
+BLOG_TITLE="My Blog" # TODO: customize
+BLOG_DESC="A minimalistic blog" # TODO: customize
+BLOG_URL="https://sam543381.github.io/ssg/" # TODO: customize
 
 get_name() {
 	echo "$1" | awk ' { sub(/^src\/[0-9]+-?/, "") ; sub(/.md$/, "") ; print } '
 }
 
-rm -f build/*.xml build/*.html
+append() {
+	[ -f "$1" ] || return 
+	awk -v bt="$BLOG_TITLE" -v bd="$BLOG_DESC" -v bu="$BLOG_URL" '{ sub(/BLOG_TITLE/, bt) ; sub(/BLOG_DESC/, bd) ; sub(/BLOG_URL/, bu) ; print }' "$1" >> "$2"
+}
+
+rm -Rf build
 mkdir -p build
+
+[ -f "src/rss-header.xml" ] && append "src/rss-header.xml" "build/feed.rss"
+append "src/header.html" "build/index.html"
+
+# shellcheck disable=SC2030
 find src/ -name "*.md" -type f | while read -r file ; do
 
-name="$(get_name "$file")"
+	name="$(get_name "$file")"
 
-	[ ! -f src/header.html ] || cat src/header.html > "build/$name.html"
+	# Article page
+	append "src/header.html" "build/$name.html"
 	./parser.sh < "$file"  >> "build/$name.html"
-	[ ! -f src/footer.html ] || cat src/footer.html >> "build/$name.html"
+	append "src/footer.html" "build/$name.html"
 
+	# Index page
+	BEGINNING="$( head -qc 1000 "$file" | ./parser.sh)"
+	echo "$BEGINNING" >> "build/index.html"
+	echo "<hr><i>Read more by clicking</i> <b><a href=\"$name.html\">this link</a></b>.<hr>" >> "build/index.html"
+
+	# RSS feed
+	if [ "$1" != "--no-rss" ] && [ "$2" != "--no-rss"  ] ; then
+		{
+		printf "<item>\n"
+		printf "\t<title>%s</title>\n" "$(awk '/^#[[:space:]]{0,}/ { sub(/^#[[:space:]]{0,}/, "") ; print}' "$file" | head -n 1)"
+			printf "\t<link>%s</link>\n" "$BLOG_URL/$name.html"
+			printf "\t<content:encoded><![CDATA[%s]]></content:encoded>\n" "$BEGINNING"
+		printf "</item>\n"
+		} >> "build/feed.rss"	
+	fi
 done
 
-
-[ "$1" != "--no-rss" ] && [ "$2" != "--no-rss"  ] && [ -f src/rss-header.xml ] && cat src/rss-header.xml > "build/rss.xml"
-
-find src/ -name "*.md" -type f | sort -r | while read -r file ; do
-
-name="$(get_name "$file")"
-
-[ ! -f src/header.html ] || cat src/header.html > "build/index.html"
-
-# shellcheck disable=SC2002
-BEGINNING="$( (cat "$file" | head --lines 5) | ./parser.sh)"
-echo "$BEGINNING" >> "build/index.html"
-
-TITLE="$(awk '/^#[[:space:]]{0,}/ { sub(/^#[[:space:]]{0,}/, "") ; print }' "$file" | head -n 1)"
-
-if [ "$1" != "--no-rss" ] && [ "$2" != "--no-rss"  ] ; then
-	echo "<item><title>$TITLE</title><link>http://myblog.com/$name.html</link><description>$BEGINNING</description></item>" >> "build/rss.xml"
-fi
-
-echo "<hr><i>Read more by clicking</i> <b><a href=\"$name.html\">this link</a></b>.<hr>" >> "build/index.html"
-
-[ ! -f src/footer.html ] || cat src/footer.html >> "build/index.html"
-
-done
-
-[ "$1" != "--no-rss" ] && [ "$2" != "--no-rss"  ] && [ -f src/rss-footer.xml ] && cat src/rss-footer.xml >> "build/rss.xml"
+append "src/footer.html" "build/index.html"
+[ -f "src/rss-footer.xml" ] && append "src/rss-footer.xml" "build/feed.rss"
